17215412917cfc3bf81e38c8ff602823ca463a98
Chris Garcia <pixelbandito@gmail.com>
2018-12-10 14:08:09 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+class UploadImage extends Component {
+  logoFormData = new FormData()
+  uploadRequest = new XMLHttpRequest();
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.uploadRequest.open('POST', 'INSERT HERE', true);
+    this.uploadRequest.setRequestHeader('authorization', 'INSERT HERE');
+    this.uploadRequest.send(this.logoFormData);
+  }
+
+  handleChange = (event) => {
+    const files = (event && event.target && event.target.files) || {};
+    const file = files.length === 1 ? files[0] : null;
+
+    if (!file) {
+      return;
+    }
+
+    this.logoFormData = new FormData()
+    this.logoFormData.append('image', file);
+    this.logoFormData.append('maxSize', 480);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          onChange={this.handleChange}
+          type="file"
+        />
+        <button>
+          Upload
+        </button>
+      </form>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <UploadImage/>
         </header>
       </div>
     );
